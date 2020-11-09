1ef1bd0b15fd4b758095c8ad69344737409479dd
unknown <petter.fogel@medieinstitutet.se>
2020-11-09 19:34:29 +0100

    Added logic for new function, when scrolling info appears.

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 window.addEventListener("load", main);
+window.addEventListener("scroll", scrollFunctions);
 
 function main()  {
     burgerIconNav();
+}
+
+function scrollFunctions() {
+    infoScrollAppear();
 }
 
 function burgerIconNav() {
@@ -10,32 +15,54 @@ function burgerIconNav() {
     const navBar = document.querySelector(".nav-links");
     // Click burger-icon to start function.
     burgerIcon.addEventListener("click", function() {
-        // Navigation Appear.
+        // Nav links shows.
         navBar.classList.toggle("nav-active");
         // Burger bars rotate.
         burgerIcon.classList.toggle("rotate");
     });
 }
 
-// Navigation Scroll Effect
+function infoScrollAppear() {
+    const aboutText = document.querySelector(".about-info");
+    const introPosition = aboutText.getBoundingClientRect().top;
+    const screenPosition = window.innerHeight / 1.8;
+
+    if (introPosition < screenPosition) {
+        aboutText.classList.add("info-appear");
+    }
+}
+
+// Navigation bar gets color when scroll-effect.
+window.onscroll = function() {
+// Fetch classes from HTML.
 const nav = document.querySelector("nav");
 const logo = document.querySelector(".logo");
 const allNavLinks = document.querySelectorAll(".nav-links li a");
-// 
-window.onscroll = function() {
     var top = window.scrollY;
-    console.log(top);
     if (top >= 50) {
         nav.classList.add("header-active");
         logo.classList.add("color-active");
+        // Loops through all links.
         for (const link of allNavLinks) {
             link.classList.add("color-active");
         }
     } else {
         nav.classList.remove("header-active");
         logo.classList.remove("color-active");
+        // Loops through all links.
         for (const link of allNavLinks) {
             link.classList.remove("color-active");
         }
     }
 }
+
+// // SMOOTH SCROLL FUNCTIONALITY
+// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+//     anchor.addEventListener('click', function(e) {
+//       e.preventDefault();
+//       document.querySelector(this.getAttribute('href')).scrollIntoView({
+//         behavior: 'smooth'
+//       });
+//     });
+//   });
+

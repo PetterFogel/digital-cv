f5cbc070140d4c274b5fd26e4ec306351c43b2f7
Petter <petter.fogel@medieinstitutet.se>
2021-02-18 19:13:19 +0100

    Put landning page logic in seperate file.

---
+++ javascript/landingPage.js
@@ -0,0 +1,48 @@
+function landingPageEffects() {
+    burgerMenuNav();
+    window.addEventListener("scroll", scrollFunctions);
+}
+
+/** Will start functions with scroll functionality. */
+function scrollFunctions() {
+    onScrollDivAppear();
+    navTransparentToColor();
+}
+
+/** Will toggle slide navigation links in and out from browser. */
+function burgerMenuNav() {
+    /** @type {HTMLDivElement} */
+    const burgerIcon = document.querySelector(".burger-menu");
+    /** @type {HTMLUListElement} */
+    const navLinks = document.querySelector(".nav-links");
+    
+    burgerIcon.addEventListener("click", function() {
+        navLinks.classList.toggle("burger-active");
+        burgerIcon.classList.toggle("rotate");
+    });
+}
+
+/** Will make a div, containing text and buttons appear when scrolling. */
+function onScrollDivAppear() {
+    /** @type {HTMLDivElement} */
+    const aboutInfoText = document.querySelector(".about-info");
+    const introPosition = aboutInfoText.getBoundingClientRect().top;
+    const screenPosition = window.innerHeight / 2;
+
+    if (introPosition < screenPosition) {
+        aboutInfoText.classList.add("info-appear");
+    }
+}
+
+/** The transparent navigation bar will get color when scrolling. */
+function navTransparentToColor() {
+    /** @type {HTMLDivElement} */
+    const navBar = document.querySelector("nav");
+    const top = window.scrollY;
+
+    if (top >= 50) {
+        navBar.classList.add("nav-active");
+    } else {
+        navBar.classList.remove("nav-active");
+    }
+}
